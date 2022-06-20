3e3728e1d790733a46027aab470f977b003f9afd
NicoRuedaA <34022939+NicoRuedaA@users.noreply.github.com>
2022-06-20 16:07:08 +0200

    3 escenas creadas
    Con la practica individual de olaia se accede a cada escena con un boton

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
-//importamos el archivo del juego
-import { Game } from './game.js';
 
-let pociones = 0;
-
-const config = {
-    //canvas o webgl
-
-
-  //var w : 2560;
-  //var h = 640;
-
-  type: Phaser.AUTO,
-  width: 1920,
-  height: 1080,
-  scene: [Game],
-  //definicion del sistema de fisicas
-  physics: {
-    default: 'arcade',
-    arcade: {
-      gravity: { y: 400 },
-      debug: false
-    }
-  }
+function phaser_game(){
+	loadpage("./html/phasergame.html");
+}
+function phaser_game2(){
+	loadpage("./html/phasergame2.html");
+}
+function phaser_game3(){
+	loadpage("./html/phasergame3.html");
+}
+function puntuaciones(){
+	loadpage("./html/puntuacions.html");
 }
 
-//instanciamos juego con su configuracion
-var game = new Phaser.Game(config);
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+}
+
+function options(){
+	loadpage("./html/options.html");
+}
+
+function load(){
+	loadpage("./html/load.html");
+}
+
